e9d41540006e1efda7e89ac6e214e17aa9becb78
Lyndee Hoge <lyndee.hoge@ou.edu>
2019-02-07 15:43:04 -0600

    Fixed Point and fixed tests

--- src/PointTest.java
+++ src/PointTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 public class PointTest {
 
+	private static final int CONST_X = 7;
+
 	@Test
 	public void testPoint() {
 		Point p = new Point();
@@ -14,17 +16,17 @@ public class PointTest {
 
 	@Test
 	public void testPointIntInt() {
-		Point p = new Point(0, 0);
-		int expected = 0;
+		Point p = new Point(CONST_X, 0);
+		int expected = CONST_X;
 		int actual = p.getX();
 		assertEquals(expected, actual);
 	}
 	
 	@Test
 	public void testPointPoint() {
-		Point p = new Point(0, 0);
+		Point p = new Point(CONST_X, 0);
 		Point x = new Point(p);
-		int expected = 0;
+		int expected = CONST_X;
 		int actual = p.getX();
 		assertEquals(expected, actual);
 	}
